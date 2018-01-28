197e399dd4503df4737a2d80648d8d07faead837
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-01-28 11:54:19 +0100

    inn

---
+++ src/Klient.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Klient
+{
+    static Socket s;
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            s = new Socket("192.168.0.13",4255);
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            while(true);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Problem z połączeniem się z bazą");
+        }
+    }
+}
