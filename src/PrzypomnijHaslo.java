99a21fb93b40132e15b2666333c3780853f24b63
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-01-30 10:45:59 +0100

    Hasło na email

--- src/PrzypomnijHaslo.java
+++ src/PrzypomnijHaslo.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class PrzypomnijHaslo {
@@ -21,6 +25,12 @@ public class PrzypomnijHaslo {
                 menu.go(frame);
             }
         });
+        Przypomnij.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                przypomnij();
+            }
+        });
     }
 
     public void go(JFrame frame1) {
@@ -28,5 +38,36 @@ public class PrzypomnijHaslo {
         frame.setTitle("Przypominanie hasła");
         frame.setContentPane(new PrzypomnijHaslo(socket).Przypomnienie);
         frame.pack();
+    }
+
+    private void przypomnij ()
+    {
+        try
+        {
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            out.println("przypomnienie");
+            String email=PoleEmail.getText();
+            if (email.equals(""))
+            {
+                Komunikat.setText("Pole Email jest puste");
+                return;
+            }
+            out.println(email);
+            out.flush();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String odpowiedz=in.readLine();
+            if (odpowiedz.equals("poprawne"))
+            {
+                Komunikat.setText("Wysłano email z hasłem na podany adres");
+            }
+            else
+            {
+                Komunikat.setText("Brak podanego adresu email w bazie danych");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
