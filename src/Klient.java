b904921e5487efaccc556c72cf977494da7a0da5
Przemysław Pajestkaw <przemek1793@gmail.com>
2018-02-08 09:29:25 +0100

    scheduler

--- src/Klient.java
+++ src/Klient.java
@@ -3,15 +3,25 @@ import java.net.Socket;
 
 public class Klient
 {
-    static Socket s;
+    static Socket sock;
 
     public static void main(String[] args)
     {
         try
         {
-            s = new Socket("192.168.0.13",4255);
-            MainMenu Menu= new MainMenu(s);
-            Menu.go();
+            Socket s = new Socket("192.168.0.13",4255);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String ip = in.readLine();
+            if (ip.equals("Brak serwerów"))
+            {
+                System.out.println("Brak dostępnych serwerów");
+            }
+            else
+            {
+                sock = new Socket(ip,4355);
+                MainMenu Menu= new MainMenu(sock);
+                Menu.go();
+            }
         }
         catch (IOException ex)
         {
